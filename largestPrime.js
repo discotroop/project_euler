8153074190365310524b53344adc4888000b520d
John Mark Goodson <john.mark.hat@gmail.com>
2020-07-06 17:19:19 -0400

    refector

--- largestPrime.js
+++ largestPrime.js
@@ -4,27 +4,70 @@
 
 let target = 600851375143;
 let testTarget = 13195;
-
+let testTargetCount = 13195;
 // Reduce range by checking square root of target.
 // Any number larger than this will multiply to a number larger than the target
 
-console.log(Math.floor(Math.sqrt(target)))
+// console.log(Math.floor(Math.sqrt(target)))
+
+// Theory of Arithmetic:
+// Any integer greater than 1 is either a prime number, or can be written as 
+// a unique product of prime numbers (ignoring the order).
+
+// 6897,,, but not working ?
 
 let starter = []
+let largest = 0;
 
-
-for (let i = 2; i < 100; i++) {
-    starter.push(i)
+let counter = 2;
+// allow for square route
+while (counter * counter <= target) {
+    if (target % counter === 0) {
+        target = target / counter;
+        largest = counter;
+    } else {
+        counter++
+    }
+}
+if (testTarget > largest) {
+    largest = target;
 }
 
-function sieve() {
-    let i = 0;
-    starter.filter(function(num) {
-        return num % starter[i] === 0;
-    })
-}
+console.log(counter);
+console.log(largest)
 
-console.log(starter)
+
+
+// for (let i = 2; i < testTarget; i++) {
+//     starter.push(i)
+// }
+
+// function sieve(index, arr) {
+//     let newArr = []
+//     for (let i = 0; i < index; i++) {
+//         newArr.push(arr[i]);
+//     }
+//     for (let i = index + 1; i < arr.length; i++) {
+//         if (arr[i] % arr[index] !== 0) {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     console.log(newArr);
+//     return newArr;
+// } 
+
+// console.log(4 % 2)
+
+// function recursiveSieve(arr) {
+//     let j = 0;
+//     while (j < arr.length) {
+//         arr = sieve(j, arr);
+//         j++
+//     }
+//     console.log("helo", arr);
+// }
+
+// recursiveSieve(starter)
 
 // sieve of erastothenes
 // generate primes
@@ -41,5 +84,3 @@ console.log(starter)
 // }
 
 // generatePrimes(testTarget)
-
-console.log(13195 % 29);
